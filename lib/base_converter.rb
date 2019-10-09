9748cc264b09c2aa7ebe4b6ec18dc061a6c14b75
Nimesh Bhakta <ncbhakta@gmail.com>
2019-10-09 01:48:22 +0100

    Implement BaseConverter#base_10_to_62

--- lib/base_converter.rb
+++ lib/base_converter.rb
@@ -1,5 +1,20 @@
 module BaseConverter
+  BASE_62_DIGITS = [('0'..'9'), ('A'..'Z'), ('a'..'z')].map(&:to_a).flatten.freeze
 
   def base_10_to_62(base10)
+    dividend = base10.to_i
+
+    return '0' if dividend == 0
+
+    base62 = ''
+
+    while dividend > 0
+      remainder = dividend % 62
+      digit = BASE_62_DIGITS[remainder]
+      base62.prepend(digit)
+      dividend /= 62
+    end
+
+    base62
   end
 end
--- spec/base_converter_spec.rb
+++ spec/base_converter_spec.rb
@@ -1,4 +1,13 @@
+require 'base_converter'
+
 describe BaseConverter do
 
-  it 'converts base 10 to base 62'
+  subject { Object.new.extend(described_class) }
+
+  describe '#base_10_to_62' do
+    it 'converts base 10 to base 62' do
+      expect(subject.base_10_to_62('0')).to eq('0')
+      expect(subject.base_10_to_62('33536566819')).to eq('abc123')
+    end
+  end
 end
